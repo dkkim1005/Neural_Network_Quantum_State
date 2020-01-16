4d5dc3ac66640fb532d441f3dd94c3bda81844f9
dkkim <dkkim1005@gist.ac.kr>
2020-01-16 11:39:04 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -35,21 +35,21 @@ int main(int argc, char* argv[])
   argsparse parser(argc, argv, options, defaults);
 
   const int nInputs = parser.find<int>("nv"),
-		    nHiddens = parser.find<int>("nh"),
-		    nChains = parser.find<int>("ns"),
-			nWarmup = parser.find<int>("nwarm"),
-			nMonteCarloSteps = parser.find<int>("nms"),
-			nIterations =  parser.find<int>("niter"),
-	        num_omp_threads = parser.find<int>("nthread"),
+            nHiddens = parser.find<int>("nh"),
+            nChains = parser.find<int>("ns"),
+            nWarmup = parser.find<int>("nwarm"),
+            nMonteCarloSteps = parser.find<int>("nms"),
+            nIterations =  parser.find<int>("niter"),
+            num_omp_threads = parser.find<int>("nthread"),
             version = parser.find<int>("ver");
   const double h = parser.find<double>("h"),
-		      J = parser.find<double>("J"),
-			  lr = parser.find<double>("lr");
+               J = parser.find<double>("J"),
+               lr = parser.find<double>("lr");
   const unsigned long seed = parser.find<unsigned long>("seed");
   const std::string path = parser.find<>("path") + "/",
-					nvstr = std::to_string(nInputs),
-					nhstr = std::to_string(nHiddens),
-					vestr = std::to_string(version);
+                    nvstr = std::to_string(nInputs),
+                    nhstr = std::to_string(nHiddens),
+                    vestr = std::to_string(version);
   std::string hfstr = std::to_string(h);
   hfstr.erase(hfstr.find_last_not_of('0') + 1, std::string::npos);
   hfstr.erase(hfstr.find_last_not_of('.') + 1, std::string::npos);
