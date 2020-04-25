d2229de0271141c34a501c82f053f95c07196441
donkyukim <dkkim1005@gist.ac.kr>
2020-04-25 22:34:43 +0900

    Added linear solvers based on the Cholesky factorization

--- gpu/linear_solver.cuh
+++ gpu/linear_solver.cuh
@@ -51,4 +51,53 @@ private:
   thrust::complex<float> * dworks_dev_;
   thrust::complex<double> * dworkd_dev_;
 };
+
+template <typename FloatType>
+class cudaCF
+{
+public:
+  cudaCF(const int n, const bool hasInitializedMagma = false):
+    kHasInitializedMagma(hasInitializedMagma),
+    kn(n)
+  {
+    if (!hasInitializedMagma)
+      CHECK_ERROR(MAGMA_SUCCESS, magma_init());
+  }
+
+  ~cudaCF()
+  {
+    if (!kHasInitializedMagma)
+      CHECK_ERROR(MAGMA_SUCCESS, magma_finalize());
+  }
+
+  void solve(thrust::complex<FloatType> * A_dev, thrust::complex<FloatType> * B_dev, thrust::complex<FloatType> * x_dev, magma_uplo_t uplo = MagmaLower);
+
+private:
+  const bool kHasInitializedMagma;
+  const int kn;
+};
+
+template <>
+void cudaCF<float>::solve(thrust::complex<float> * A_dev, thrust::complex<float> * B_dev, thrust::complex<float> * x_dev, magma_uplo_t uplo)
+{
+  magma_int_t info;
+  CHECK_ERROR(MAGMA_SUCCESS, magma_cposv_gpu(uplo, kn, 1,
+    reinterpret_cast<magmaFloatComplex*>(A_dev), kn,
+    reinterpret_cast<magmaFloatComplex*>(B_dev), kn, &info));
+  if (info < 0)
+    throw std::runtime_error("output of magma_cposv_gpu: info=" + std::to_string(info));
+  CHECK_ERROR(cudaSuccess, cudaMemcpy(x_dev, B_dev, sizeof(thrust::complex<float>)*kn, cudaMemcpyDeviceToDevice));
+}
+
+template <>
+void cudaCF<double>::solve(thrust::complex<double> * A_dev, thrust::complex<double> * B_dev, thrust::complex<double> * x_dev, magma_uplo_t uplo)
+{
+  magma_int_t info;
+  CHECK_ERROR(MAGMA_SUCCESS, magma_zposv_gpu(uplo, kn, 1,
+    reinterpret_cast<magmaDoubleComplex*>(A_dev), kn,
+    reinterpret_cast<magmaDoubleComplex*>(B_dev), kn, &info));
+  if (info < 0)
+    throw std::runtime_error("output of magma_zposv_gpu: info=" + std::to_string(info));
+  CHECK_ERROR(cudaSuccess, cudaMemcpy(x_dev, B_dev, sizeof(thrust::complex<double>)*kn, cudaMemcpyDeviceToDevice));
+}
 } // namespace linearsolver
