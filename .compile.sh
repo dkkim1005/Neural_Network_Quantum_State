8d3622c0ba54ccc6b12bf72c186033c2e239369b
dongkyukim <dkkim@gmail.com>
2020-04-30 12:20:03 +0000

    replaced variables related to MKL with 'pkg-config'

--- .compile.sh
+++ .compile.sh
@@ -7,9 +7,7 @@ CFLAGS="-O3 -std=c++11"
 CC=DEFAULT # CUDA nvcc compiler
 TRNG4_INC_PATH=DEFAULT
 TRNG4_LIB_PATH=DEFAULT
-MKL_INC_PATH=DEFAULT
-MKL_LIB_PATH1=DEFAULT
-MKL_LIB_PATH2=DEFAULT
+MAGMA_PKG_CONFIG_PATH=DEFAULT
 MAGMA_INC_PATH=DEFAULT
 MAGMA_LIB_PATH=DEFAULT
 CUDA_INC_PATH=DEFAULT
@@ -32,17 +30,10 @@ fi
 if [ $TRNG4_LIB_PATH = "DEFAULT" ]; then
   TRNG4_LIB_PATH=/usr/local/lib
 fi
-# MKL
-if [ $MKL_INC_PATH = "DEFAULT" ]; then
-  MKL_INC_PATH=/opt/intel/mkl/include
-fi
-if [ $MKL_LIB_PATH1 = "DEFAULT" ]; then
-  MKL_LIB_PATH1=/opt/intel/mkl/lib/intel64
-fi
-if [ $MKL_LIB_PATH2 = "DEFAULT" ]; then
-  MKL_LIB_PATH2=/opt/intel/compilers_and_libraries/linux/lib/intel64
-fi
 # MAGMA-GPU
+if [ $MAGMA_PKG_CONFIG_PATH = "DEFAULT" ]; then
+  MAGMA_PKG_CONFIG_PATH=/usr/local/magma/lib/pkgconfig
+fi
 if [ $MAGMA_INC_PATH = "DEFAULT" ]; then
   MAGMA_INC_PATH=/usr/local/magma/include
 fi
@@ -59,8 +50,7 @@ fi
 #------------------------------
 
 TRNG4_FLAGS="-ltrng4 -I$TRNG4_INC_PATH -L$TRNG4_LIB_PATH"
-MKL_FLAGS="-I$MKL_INC_PATH -L$MKL_LIB_PATH1 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -L$MKL_LIB_PATH2 -liomp5 -lpthread -lm -ldl"
-MAGMA_FLAGS="-Xcompiler -fopenmp -DMIN_CUDA_ARCH=$MIN_CUDA_ARCH -I$MAGMA_INC_PATH -I$CUDA_INC_PATH -L$MAGMA_LIB_PATH -L$CUDA_LIB_PATH -lmagma_sparse -lmagma -lcublas -lcusparse -lcudart -lcudadevrt"
+MAGMA_FLAGS="-DMIN_CUDA_ARCH=$MIN_CUDA_ARCH -I$MAGMA_INC_PATH -I$CUDA_INC_PATH -L$MAGMA_LIB_PATH -L$CUDA_LIB_PATH `pkg-config $MAGMA_PKG_CONFIG_PATH/magma.pc --libs | sed 's/-fopenmp/-Xcompiler -fopenmp/g'`"
 
 # name of the bash script
 SCRIPT_NAME=$(basename "$0")
@@ -75,7 +65,7 @@ for SRC in ${SRCS[@]}; do
     continue
   fi
   echo -e "\e[1;7;32mCompiling... ($SRC => $TARGET)\e[0m"
-  CMD="$CC -o $TARGET $SRC_PATH $CFLAGS $MAGMA_FLAGS $MKL_FLAGS $TRNG4_FLAGS"
+  CMD="$CC -o $TARGET $SRC_PATH $CFLAGS $MAGMA_FLAGS $TRNG4_FLAGS"
   echo $CMD
   eval $CMD
 done
