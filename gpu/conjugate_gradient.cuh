345b620a221ca7945a6e55c3ca4fe541d0066584
donkyukim <dkkim1005@gist.ac.kr>
2020-06-10 16:58:27 +0900

    gathered utilities of thrust into 'thrust_util.cuh'

--- gpu/conjugate_gradient.cuh
+++ gpu/conjugate_gradient.cuh
@@ -4,70 +4,7 @@
 
 #include <iostream>
 #include <cmath>
-#include <thrust/complex.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/functional.h>
-#include <thrust/transform_reduce.h>
-#include <thrust/transform.h>
-#include <thrust/inner_product.h>
-
-namespace internal_impl
-{
-template <typename FloatType>
-struct NormFunctor
-{
-  __host__ __device__ FloatType operator()(const thrust::complex<FloatType> & x) const
-  {
-    return thrust::norm(x);
-  }
-};
-
-template <typename FloatType>
-struct aDotbFunctor: public thrust::binary_function<thrust::complex<FloatType>,
-  thrust::complex<FloatType>, thrust::complex<FloatType>>
-{
-  __host__ __device__ thrust::complex<FloatType> operator()(thrust::complex<FloatType> a, thrust::complex<FloatType> b) const
-  {
-    return a*thrust::conj(b);
-  }
-};
-
-template <typename FloatType>
-struct AxpyFunctor: public thrust::binary_function<thrust::complex<FloatType>, thrust::complex<FloatType>, thrust::complex<FloatType>>
-{
-  AxpyFunctor(const thrust::complex<FloatType> alpha, const thrust::complex<FloatType> beta = 1):
-    kalpha(alpha),
-    kbeta(beta)
-  {}
-
-  __host__ __device__ thrust::complex<FloatType> operator()(const thrust::complex<FloatType> & a, const thrust::complex<FloatType> & b) const
-  {
-    return (kalpha*a)+(kbeta*b);
-  }
-
-private:
-  const thrust::complex<FloatType> kalpha, kbeta;
-};
-
-// return \sum_i |v_i|^2
-template <typename FloatType>
-FloatType squared_norm(const thrust::device_vector<thrust::complex<FloatType>> & v)
-{
-  const FloatType zero = 0;
-  return thrust::transform_reduce(v.begin(), v.end(), NormFunctor<FloatType>(), zero, thrust::plus<FloatType>());
-}
-
-// return \sum_i (a_i*conj(b_i))
-template <typename FloatType>
-thrust::complex<FloatType> hermition_inner_product(const thrust::device_vector<thrust::complex<FloatType>> & a,
-  const thrust::device_vector<thrust::complex<FloatType>> & b)
-{
-  const thrust::complex<FloatType> kzero = thrust::complex<FloatType>(0, 0);
-  return thrust::inner_product(a.begin(), a.end(), b.begin(), kzero, thrust::plus<thrust::complex<FloatType>>(), aDotbFunctor<FloatType>());
-}
-} // internal_impl
-
+#include "thrust_util.cuh"
 
 /*
   GPU-ConjugateGradient for solving a Hermition matrix 
