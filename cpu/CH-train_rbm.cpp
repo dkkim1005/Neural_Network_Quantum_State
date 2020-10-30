5b1163b0e9f949c04caa565492ae6dc1f2575c23
donkyukim <dkkim1005@gist.ac.kr>
2020-10-30 17:16:54 +0900

    edited CH-train_rbm.cpp

--- cpu/CH-train_rbm.cpp
+++ cpu/CH-train_rbm.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2020 Dongkyu Kim (dkkim1005@gmail.com)
 
+#define NO_USE_BATCH
 #include <chrono>
 #include "hamiltonians.hpp"
 #include "optimizer.hpp"
@@ -9,10 +10,9 @@ int main(int argc, char* argv[])
 {
   std::vector<pair_t> options, defaults;
   // env; explanation of env
-  options.push_back(pair_t("nv", "# of visible nodes"));
+  options.push_back(pair_t("ninput", "# of visible nodes"));
   options.push_back(pair_t("nh", "# of hidden nodes"));
   options.push_back(pair_t("ns", "# of spin samples for parallel Monte-Carlo"));
-  options.push_back(pair_t("na", "# of iterations to average out observables"));
   options.push_back(pair_t("niter", "# of iterations to train RBM"));
   options.push_back(pair_t("h", "transverse-field strength"));
   options.push_back(pair_t("ver", "version"));
@@ -36,24 +36,23 @@ int main(int argc, char* argv[])
   // parser for arg list
   argsparse parser(argc, argv, options, defaults);
 
-  const int nInputs = parser.find<int>("nv"),
-            nHiddens = parser.find<int>("nh"),
-            nChains = parser.find<int>("ns"),
-            nAccumulation = parser.find<int>("na"),
-            nWarmup = parser.find<int>("nwarm"),
-            nMonteCarloSteps = parser.find<int>("nms"),
-            nIterations =  parser.find<int>("niter"),
-            num_omp_threads = parser.find<int>("nthread"),
-            version = parser.find<int>("ver");
+  const int nInputs = parser.find<int>("ninput"),
+    nHiddens = parser.find<int>("nh"),
+    nChains = parser.find<int>("ns"),
+    nWarmup = parser.find<int>("nwarm"),
+    nMonteCarloSteps = parser.find<int>("nms"),
+    nIterations =  parser.find<int>("niter"),
+    num_omp_threads = parser.find<int>("nthread"),
+    version = parser.find<int>("ver");
   const double h = parser.find<double>("h"),
-               J = parser.find<double>("J"),
-               lr = parser.find<double>("lr");
+    J = parser.find<double>("J"),
+    lr = parser.find<double>("lr");
   const unsigned long seed = parser.find<unsigned long>("seed");
   const std::string path = parser.find<>("path") + "/",
-                    nvstr = std::to_string(nInputs),
-                    nhstr = std::to_string(nHiddens),
-                    vestr = std::to_string(version),
-                    ifprefix = parser.find<>("ifprefix");
+    nvstr = std::to_string(nInputs),
+    nhstr = std::to_string(nHiddens),
+    vestr = std::to_string(version),
+    ifprefix = parser.find<>("ifprefix");
   std::string hfstr = std::to_string(h);
   hfstr.erase(hfstr.find_last_not_of('0') + 1, std::string::npos);
   hfstr.erase(hfstr.find_last_not_of('.') + 1, std::string::npos);
@@ -74,19 +73,19 @@ int main(int argc, char* argv[])
 
   // block size for the block splitting scheme of parallel Monte-Carlo
   const unsigned long nBlocks = static_cast<unsigned long>(nIterations)*
-                                static_cast<unsigned long>(nMonteCarloSteps)*
-                                static_cast<unsigned long>(nInputs)*
-                                static_cast<unsigned long>(nChains);
+    static_cast<unsigned long>(nMonteCarloSteps)*
+    static_cast<unsigned long>(nInputs)*
+    static_cast<unsigned long>(nChains);
 
   // Transverse Field Ising Hamiltonian with 1D chain system
-  spinhalf::TFIChain<AnsatzTraits<Ansatz::RBM, double> > rbmWrapper(machine, h, J, nBlocks, seed);
+  spinhalf::TFIChain<AnsatzTraits<Ansatz::RBM, double> > sampler(machine, h, J, nBlocks, seed);
   const auto start = std::chrono::system_clock::now();
 
-  rbmWrapper.warm_up(nWarmup);
+  sampler.warm_up(nWarmup);
 
   // imaginary time propagator
-  StochasticReconfiguration<double, linearsolver::BKF> iTimePropagator(nChains, machine.get_nVariables());
-  iTimePropagator.propagate(rbmWrapper, nIterations, nAccumulation, nMonteCarloSteps, lr);
+  StochasticReconfigurationCG<double> iTimePropagator(nChains, machine.get_nVariables());
+  iTimePropagator.propagate(sampler, nIterations, nMonteCarloSteps, lr);
 
   const auto end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
